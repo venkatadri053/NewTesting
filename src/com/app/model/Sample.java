837f1147b99b8ac099adf668c5e953fe92c24d19
KESANI.VENKATADRI <35061636+venkatadri053@users.noreply.github.com>
2018-12-02 19:21:35 +0530

    added sommmmmme

--- src/com/app/model/Sample.java
+++ src/com/app/model/Sample.java
@@ -3,5 +3,7 @@ package com.app.model;
 public class Sample {
  public static void main(String[] args) {
 	System.out.println("Welcome To New Repo");
+	 System.out.println("Welcome To Venky");
+	 System.out.println("Welcome To Working properly");
 }
 }
